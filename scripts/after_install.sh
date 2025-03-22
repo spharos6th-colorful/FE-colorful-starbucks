fd3796054f80a000e5053334597e080f60aa84f0
HoChoRoo <rttkhc0304@gmail.com>
2025-03-23 00:05:38 +0900

    :package: chore: CICD 중 ec2에서 파일 경로 절대경로로 변경, 명령어에 sudo로 권한 추가

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,7 +1,7 @@
 mkdir -p /home/ubuntu/colorful-starbucks-frontend
 cd /home/ubuntu/colorful-starbucks-frontend || exit 1
 
-RUNNING_CONTAINER=$(docker ps --filter "name=nextjs-blue" --format "{{.ID}}")
+RUNNING_CONTAINER=$(sudo docker ps --filter "name=nextjs-blue" --format "{{.ID}}")
 if [ -n "$RUNNING_CONTAINER" ]; then
   DEPLOY_ENV="green"
   TARGET_PORT=3001
@@ -15,18 +15,14 @@ fi
 LATEST_IMAGE=$(aws ecr describe-images --repository-name colorful-stabucks-frontend --query 'sort_by(imageDetails,& imagePushedAt)[-1].imageTags[0]' --output text --region ap-northeast-2)
 FULL_IMAGE_NAME="065676097951.dkr.ecr.ap-northeast-2.amazonaws.com/colorful-stabucks-frontend:${LATEST_IMAGE}"
 
-touch docker-compose.yml
-chmod 644 docker-compose.yml
+echo "version: '3'" > /home/ubuntu/colorful-starbucks-frontend/docker-compose.yml
+echo "services:" >> /home/ubuntu/colorful-starbucks-frontend/docker-compose.yml
+echo "  nextjs-${DEPLOY_ENV}:" >> /home/ubuntu/colorful-starbucks-frontend/docker-compose.yml
+echo "    image: ${FULL_IMAGE_NAME}" >> /home/ubuntu/colorful-starbucks-frontend/docker-compose.yml
+echo "    container_name: nextjs-${DEPLOY_ENV}" >> /home/ubuntu/colorful-starbucks-frontend/docker-compose.yml
+echo "    ports:" >> /home/ubuntu/colorful-starbucks-frontend/docker-compose.yml
+echo "      - \"${TARGET_PORT}:3000\"" >> /home/ubuntu/colorful-starbucks-frontend/docker-compose.yml
+echo "    restart: always" >> /home/ubuntu/colorful-starbucks-frontend/docker-compose.yml
 
-cat > docker-compose.yml <<EOL
-version: '3'
-services:
-  nextjs-${DEPLOY_ENV}:
-    image: ${FULL_IMAGE_NAME}
-    container_name: nextjs-${DEPLOY_ENV}
-    ports:
-      - "${TARGET_PORT}:3000"
-    restart: always
-EOL
-
-sudo docker-compose -f /home/ubuntu/colorful-starbucks-frontend/docker-compose.yml up -d
+cd /home/ubuntu/colorful-starbucks-frontend
+sudo docker-compose up -d
