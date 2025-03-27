aaed45de60d404fc8d3aad0878c19c8cf28d7616
HoChoRoo <rttkhc0304@gmail.com>
2025-03-27 20:43:52 +0900

    🐛 fix: useSearchParams 쓰는 컴포넌트에 Suspense 로 감쌌음

--- src/app/(main)/products/page.tsx
+++ src/app/(main)/products/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
 import ProductCategoryTopLayout from '@/components/layouts/ProductCategoryTopLayout';
 
 const ProductsPage: React.FC = () => {
   return (
     <div className='container mx-auto py-4'>
-      <ProductCategoryTopLayout />
+      <Suspense fallback={''}>
+        {/* TODO : fallback에 스켈레톤 적용예정*/}
+        <ProductCategoryTopLayout />
+      </Suspense>
       {/* 상품 소분류 카테고리 및 옵션들이 들어갈 예정 */}
       {/* 상품 리스트 나타낼 페에지가 들어갈 예정 */}
       <div className='mt-6'>{/* <ProductList categoryId={activeCategoryId} /> */}</div>
--- src/components/ui/products/ProductActions.tsx
+++ src/components/ui/products/ProductActions.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { Button } from '../Button';
+
 // import { useRouter } from 'next/navigation';
 // import { useState } from 'react';
 
@@ -12,43 +14,21 @@ export default function ProductActions({ productId }: ProductActionsProps) {
   //const [isAdding, setIsAdding] = useState(false);
   console.log(productId); //eslint 때문에 잠시 출력만 걸었음(사용하지않는 변수 있으면 깃 커밋 불가)
 
-  const addToCart = async () => {
-    // TODO : 장바구니 추가하는 로직(api) 추가해야함
-  };
+  // const addToCart = async () => {
+  //   // TODO : 장바구니 추가하는 로직(api) 추가해야함
+  // };
 
-  const handlePurchase = async () => {
-    // 장바구니 담지 말고 바로 구매시 구매링크로 이동 예정
-    // TODO옵션 선택하는 모달이 열려야함
-    //router.push(`/checkout?products=${productId}`);
-  };
+  // const handlePurchase = async () => {
+  //   // 장바구니 담지 말고 바로 구매시 구매링크로 이동 예정
+  //   // TODO옵션 선택하는 모달이 열려야함
+  //   //router.push(`/checkout?products=${productId}`);
+  // };
 
   return (
     <div className='p-4 flex items-center'>
-      <button
-        onClick={addToCart}
-        // disabled={isAdding}
-        disabled={false}
-        className='border rounded-full p-3 mr-3'
-        aria-label='장바구니에 추가'
-      >
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='20'
-          height='20'
-          viewBox='0 0 24 24'
-          fill='none'
-          stroke='currentColor'
-          strokeWidth='2'
-        >
-          <circle cx='9' cy='21' r='1' />
-          <circle cx='20' cy='21' r='1' />
-          <path d='M1 1h4l2.68 13.39a2 2 0 002 1.61h9.72a2 2 0 002-1.61L23 6H6' />
-        </svg>
-      </button>
-      {/* TODO : 커스텀 버튼 컴포넌트로 변경 */}
-      <button onClick={handlePurchase} className='flex-1 bg-green-600 text-white py-3 px-4 rounded-full font-medium'>
+      <Button variant='default' width='full'>
         구매하기
-      </button>
+      </Button>
     </div>
   );
 }
