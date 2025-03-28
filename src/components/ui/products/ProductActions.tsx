d6df74a6004b777c649e8aabcbc000397c8faa99
HoChoRoo <rttkhc0304@gmail.com>
2025-03-28 16:23:58 +0900

    ✨ feat: 상품상세페이지 구매하기 버튼 영역(ProductActions)
    스크롤 시 숨김처리
    스크롤 멈추고 0.5초 뒤 나타나게 설정

--- src/components/ui/products/ProductActions.tsx
+++ src/components/ui/products/ProductActions.tsx
@@ -1,7 +1,5 @@
 'use client';
-// import { useRouter } from 'next/navigation';
-// import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
 import { Button } from '../Button';
 
 interface ProductActionsProps {
@@ -9,22 +7,39 @@ interface ProductActionsProps {
 }
 
 export default function ProductActions({ productId }: ProductActionsProps) {
-  //const router = useRouter();
-  //const [isAdding, setIsAdding] = useState(false);
-  console.log(productId); //eslint 때문에 잠시 출력만 걸었음(사용하지않는 변수 있으면 깃 커밋 불가)
+  console.log(productId);
+  const [isVisible, setIsVisible] = useState(true);
+  const [scrollTimeout, setScrollTimeout] = useState<NodeJS.Timeout | null>(null);
 
-  // const addToCart = async () => {
-  //   // TODO : 장바구니 추가하는 로직(api) 추가해야함
-  // };
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsVisible(false);
 
-  // const handlePurchase = async () => {
-  //   // 장바구니 담지 말고 바로 구매시 구매링크로 이동 예정
-  //   // TODO옵션 선택하는 모달이 열려야함
-  //   //router.push(`/checkout?products=${productId}`);
-  // };
+      if (scrollTimeout) {
+        clearTimeout(scrollTimeout);
+      }
+
+      const timeout = setTimeout(() => {
+        setIsVisible(true);
+      }, 500);
+
+      setScrollTimeout(timeout);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      if (scrollTimeout) clearTimeout(scrollTimeout);
+    };
+  }, [scrollTimeout]);
 
   return (
-    <div className='fixed bottom-0 left-0 right-0 p-4  z-50'>
+    <div
+      className={`fixed bottom-0 left-0 right-0 p-4 z-50 transition-all duration-500 ease-in-out ${
+        isVisible ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'
+      }`}
+    >
       <Button variant='default' width='full'>
         구매하기
       </Button>
