3a186dd3ee8fba66da3770c7faa56150998b1476
HoChoRoo <rttkhc0304@gmail.com>
2025-03-31 21:37:19 +0900

    🐛 fix: 머지 병합 충돌 수정(webpack-svgr 2번 적용된거수정)

--- next.config.ts
+++ next.config.ts
@@ -19,13 +19,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
 };
 
 export default nextConfig;
