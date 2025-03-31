941d263222051ac34be4edcd95bb583dc1ac3ff3
HoChoRoo <rttkhc0304@gmail.com>
2025-03-31 15:19:02 +0900

    🔨 refactor: categoryTopLayout -\> 서스펜스 감싸기 위해 CategoryContent.tsx
    파일 생성 후 분리

--- src/components/layouts/ProductCategoryTopLayout.tsx
+++ src/components/layouts/ProductCategoryTopLayout.tsx
@@ -1,40 +1,9 @@
 'use client';
 
 import React, { Suspense } from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
 
-import { sampleCategories } from '@/data/productCategoryTopDummyDatas';
-import { ProductCategoryTopType } from '@/types/products/productCategoryType';
-import ProductCategoryTop from '../ui/products/ProductCategoryTop';
 import CategorySkeleton from '../ui/skeleton/CategorySkeleton';
-
-function CategoryContent() {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const currentCategoryCode = searchParams.get('category') || 'all';
-
-  const handleCategoryClick = (category: ProductCategoryTopType) => {
-    const params = new URLSearchParams(searchParams.toString());
-    params.set('category', category.code);
-
-    router.push(`?${params.toString()}`, { scroll: false });
-  };
-
-  return (
-    <div className='flex overflow-x-auto'>
-      {sampleCategories.map((category) => (
-        <div key={category.code} className='flex-shrink-0'>
-          <ProductCategoryTop
-            name={category.name}
-            isActive={currentCategoryCode === category.code}
-            onClick={() => handleCategoryClick(category)}
-          />
-        </div>
-      ))}
-    </div>
-  );
-}
+import CategoryContent from '../modules/product/CategoryContent';
 
 export default function ProductCategoryTopLayout() {
   return (
