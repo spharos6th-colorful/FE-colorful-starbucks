e78cb2dc24493ed43056c82dd807990ea64d6dca
HoChoRoo <rttkhc0304@gmail.com>
2025-03-31 15:13:34 +0900

    🔨 refactor: div 태그와 button 태그 변경

--- src/components/ui/products/ProductAccordion.tsx
+++ src/components/ui/products/ProductAccordion.tsx
@@ -8,24 +8,26 @@ export default function ProductAccordion({ title, content }: ProductAccordionPro
 
   return (
     <div className='border-t border-b'>
-      <div className='py-4 flex justify-between items-center cursor-pointer' onClick={() => setIsOpen(!isOpen)}>
-        <span className='font-medium'>{title}</span>
-        <button aria-label={isOpen ? '상세정보 접기' : '상세정보 펼치기'}>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-            fill='none'
-            stroke='currentColor'
-            strokeWidth='2'
-            style={{ transform: isOpen ? 'rotate(180deg)' : 'none' }}
-            className='transition-transform duration-300'
-          >
-            <polyline points='6 9 12 15 18 9' />
-          </svg>
-        </button>
-      </div>
+      <button
+        className='py-4 w-full flex justify-between items-center cursor-pointer'
+        onClick={() => setIsOpen(!isOpen)}
+        aria-label={isOpen ? '상세정보 접기' : '상세정보 펼치기'}
+      >
+        <span className='font-medium text-left'>{title}</span>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          width='24'
+          height='24'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth='2'
+          style={{ transform: isOpen ? 'rotate(180deg)' : 'none' }}
+          className='transition-transform duration-300'
+        >
+          <polyline points='6 9 12 15 18 9' />
+        </svg>
+      </button>
 
       {isOpen && (
         <div className='px-2 pb-4'>
