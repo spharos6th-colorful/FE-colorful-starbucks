37ac21b0065a4007f59b4ecd89663098756531d7
HoChoRoo <rttkhc0304@gmail.com>
2025-03-31 17:10:14 +0900

    ✨ feat: 상품 상세 페이지 구매하기 액션 컴포넌트에 장바구니 svg 추가

--- src/components/ui/products/ProductActions.tsx
+++ src/components/ui/products/ProductActions.tsx
@@ -2,6 +2,7 @@
 
 import { useScrollVisibility } from '@/hooks/useScrollVisibility';
 import { Button } from '../common';
+import CartIcon from '@/assets/icon/product/cart.svg';
 
 interface ProductActionsProps {
   productId: string;
@@ -13,13 +14,16 @@ export default function ProductActions({ productId }: ProductActionsProps) {
 
   return (
     <div
-      className={`fixed bottom-0 left-0 right-0 p-4 z-50 transition-all duration-500 ease-in-out ${
+      className={`fixed bottom-0 left-0 right-0 p-4 z-50 bg-white rounded-t-2xl transition-all duration-500 ease-in-out ${
         isVisible ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'
       }`}
     >
-      <Button variant='default' width='full'>
-        구매하기
-      </Button>
+      <div className=' flex items-center gap-3'>
+        <CartIcon className='flex-shrink-0' />
+        <Button variant='default' className='flex-grow'>
+          구매하기
+        </Button>
+      </div>
     </div>
   );
 }
