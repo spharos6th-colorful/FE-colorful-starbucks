383db81daee927f0843fc3a0a271a631f1d93d97
HoChoRoo <rttkhc0304@gmail.com>
2025-03-31 09:48:57 +0900

    ✨ feat: api 연동 테스트 위해 s3권한 추가 및 상품 상세 조회 api 생성

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    domains: ['colorful-starbucks.s3.ap-northeast-2.amazonaws.com'],
+  },
 };
 
 export default nextConfig;
