bea73a5524fba42737e886c07a1bca54df928f21
HoChoRoo <rttkhc0304@gmail.com>
2025-03-31 21:19:23 +0900

    🐛 fix: 상품 상세 페이지에서 구매하기 모달 추가

--- src/components/ui/products/ProductActions.tsx
+++ src/components/ui/products/ProductActions.tsx
@@ -3,27 +3,45 @@
 import { useScrollVisibility } from '@/hooks/useScrollVisibility';
 import { Button } from '../common';
 import CartIcon from '@/assets/icon/product/cart.svg';
+import { useModal } from '../common/Modal';
+import PurchaseModal from './PurchaseModal';
 
 interface ProductActionsProps {
   productId: string;
+  productName: string;
+  productPrice: number;
 }
 
-export default function ProductActions({ productId }: ProductActionsProps) {
+export default function ProductActions({ productId, productPrice }: ProductActionsProps) {
+  const { modal, openModal } = useModal();
+
+  const handlePurchaseClick = () => {
+    openModal('purchase-modal');
+  };
+
   console.log(productId);
   const isVisible = useScrollVisibility(500);
 
+  const isPurchaseModalOpen = modal === 'purchase-modal';
+
   return (
-    <div
-      className={`fixed bottom-0 left-0 right-0 p-4 z-50 bg-white rounded-t-2xl transition-all duration-500 ease-in-out ${
-        isVisible ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'
-      }`}
-    >
-      <div className=' flex items-center gap-3'>
-        <CartIcon className='flex-shrink-0' />
-        <Button variant='default' className='flex-grow'>
-          구매하기
-        </Button>
-      </div>
-    </div>
+    <>
+      {!isPurchaseModalOpen && (
+        <div
+          className={`fixed max-w-3xl bottom-0 left-1/2 w-full -translate-x-1/2 p-4 z-40 bg-white rounded-t-2xl shadow-lg transition-all duration-500 ease-in-out ${
+            isVisible ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'
+          }`}
+        >
+          <div className='flex items-center gap-3 max-w-screen-lg mx-auto'>
+            <CartIcon className='flex-shrink-0' />
+            <Button variant='default' className='flex-grow' onClick={handlePurchaseClick}>
+              구매하기
+            </Button>
+          </div>
+        </div>
+      )}
+
+      <PurchaseModal productId={productId} productPrice={productPrice} />
+    </>
   );
 }
