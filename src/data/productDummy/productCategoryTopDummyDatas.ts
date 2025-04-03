5b9d5770daecf1543469d3709570dd73a3299bda
HoChoRoo <rttkhc0304@gmail.com>
2025-04-03 19:51:28 +0900

    ✨ feat: 카테고리 타입 및 더미

--- src/data/productDummy/productCategoryTopDummyDatas.ts
+++ src/data/productDummy/productCategoryTopDummyDatas.ts
@@ -1,4 +1,9 @@
-import { ProductCategoryTopType } from '@/types/products/productCategoryType';
+import {
+  FilterData,
+  ProductCategoryTopType,
+  SubDetailCategory,
+  SubSizeCateogry,
+} from '@/types/products/productCategoryType';
 
 export const sampleCategories: ProductCategoryTopType[] = [
   { topCategoryId: 1, categoryName: '전체' },
@@ -7,3 +12,36 @@ export const sampleCategories: ProductCategoryTopType[] = [
   { topCategoryId: 4, categoryName: '라이프스타일' },
   { topCategoryId: 5, categoryName: '티/커피' },
 ];
+
+export const sampleSubDetailCategories: SubDetailCategory[] = [
+  { bottomCategoryId: 1, categoryName: '플라스틱 텀블러' },
+  { bottomCategoryId: 2, categoryName: '스테인리스 텀블러' },
+  { bottomCategoryId: 3, categoryName: '보온병' },
+  { bottomCategoryId: 4, categoryName: '콜드컵' },
+];
+
+export const sampleSubVolumeCategories: SubSizeCateogry[] = [
+  { sizeId: 'short', sizeName: 'Short' },
+  { sizeId: 'tall', sizeName: 'Tall' },
+  { sizeId: 'grande', sizeName: 'Grande' },
+  { sizeId: 'venti', sizeName: 'Venti' },
+  { sizeId: 'trenta', sizeName: 'Trenta' },
+];
+
+// 시즌 필터 더미 데이터
+export const sampleSeasonOptions = [
+  { filterId: 'finish-collabo', filterName: '피니시 콜라보' },
+  { filterId: 'march-new-core', filterName: '3월 신규코어' },
+  { filterId: 'flower-market', filterName: '플라워 마켓' },
+];
+
+// 전체 필터 데이터
+export const sampleFilterData: FilterData = {
+  seasons: sampleSeasonOptions,
+  price: [
+    { filterId: 'under10000', filterName: '1만원미만' },
+    { filterId: '10000to20000', filterName: '1만원대' },
+    { filterId: '20000to30000', filterName: '2만원대' },
+    { filterId: '30000to40000', filterName: '3만원대' },
+  ],
+};
--- src/types/products/productCategoryType.ts
+++ src/types/products/productCategoryType.ts
@@ -2,3 +2,27 @@ export interface ProductCategoryTopType {
   topCategoryId: number;
   categoryName: string;
 }
+
+// 하위 카테고리 타입
+export type SubDetailCategory = {
+  bottomCategoryId: number;
+  categoryName: string;
+};
+
+// 용량 카테고리 타입
+export type SubSizeCateogry = {
+  sizeId: string;
+  sizeName: string;
+};
+
+// 필터 옵션 타입
+export type FilterOption = {
+  filterId: string;
+  filterName: string;
+};
+
+// 필터 데이터 타입
+export type FilterData = {
+  seasons: FilterOption[];
+  price: FilterOption[];
+};
