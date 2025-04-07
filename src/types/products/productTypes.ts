940d7819938a8fe55981fb4517b904e2038c8eee
HoChoRoo <rttkhc0304@gmail.com>
2025-04-07 16:36:55 +0900

    🐛 fix: productCode -\> string에서 number로 변경
    그에 따른 api 수정 및 더미데이터 생성

--- src/types/products/productTypes.ts
+++ src/types/products/productTypes.ts
@@ -7,3 +7,18 @@ export interface ProductTypes {
   markable: string;
   price: number;
 }
+
+export interface ProductItem {
+  id: number;
+  productCode: number;
+}
+
+export interface ProductListData {
+  content: ProductItem[];
+  hasNext: boolean;
+  nextCursor: string | null;
+}
+
+export interface ProductsWithDetailsData extends ProductListData {
+  productDetails: ProductTypes[];
+}
