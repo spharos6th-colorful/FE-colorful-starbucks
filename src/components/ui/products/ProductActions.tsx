0d9f0508d177188f867e4c2c7b8f81590b92eeb7
Dorabang <psa1195@naver.com>
2025-04-09 21:38:53 +0900

    🐛 fix: 이전 LegacyModal을 사용하는 곳은 import 경로 변경
    - ProductDetailPage는 변경 사항이 많아질 예정이므로, 이후에 페이지 수정과 같이 하는 것이 좋아보임

--- src/components/ui/products/ProductActions.tsx
+++ src/components/ui/products/ProductActions.tsx
@@ -3,7 +3,7 @@
 import { useScrollVisibility } from '@/hooks/useScrollVisibility';
 import { Button } from '../common';
 import CartIcon from '@/assets/icon/product/cart.svg';
-import { useModal } from '../common/Modal';
+import { useModal } from '../common/Modal/LegacyModal';
 import PurchaseModal from './PurchaseModal';
 import { ProductOptionType } from '@/types/products/productPurchaseTypes';
 
@@ -14,7 +14,12 @@ interface ProductActionsProps {
   productOptions: ProductOptionType[];
 }
 
-export default function ProductActions({ productId, productPrice, productName, productOptions }: ProductActionsProps) {
+export default function ProductActions({
+  productId,
+  productPrice,
+  productName,
+  productOptions,
+}: ProductActionsProps) {
   const { modal, openModal } = useModal();
   console.log(productName);
 
@@ -30,19 +35,29 @@ export default function ProductActions({ productId, productPrice, productName, p
       {!isPurchaseModalOpen && (
         <div
           className={`fixed max-w-3xl bottom-0 left-1/2 w-full -translate-x-1/2 p-4 z-40 bg-white rounded-t-2xl shadow-lg transition-all duration-500 ease-in-out ${
-            isVisible ? 'translate-y-0 opacity-100' : 'translate-y-full opacity-0'
+            isVisible
+              ? 'translate-y-0 opacity-100'
+              : 'translate-y-full opacity-0'
           }`}
         >
           <div className='flex items-center gap-3 max-w-screen-lg mx-auto'>
             <CartIcon className='flex-shrink-0' />
-            <Button variant='default' className='flex-grow' onClick={handlePurchaseClick}>
+            <Button
+              variant='default'
+              className='flex-grow'
+              onClick={handlePurchaseClick}
+            >
               구매하기
             </Button>
           </div>
         </div>
       )}
 
-      <PurchaseModal productId={productId} productPrice={productPrice} productOptions={productOptions} />
+      <PurchaseModal
+        productId={productId}
+        productPrice={productPrice}
+        productOptions={productOptions}
+      />
     </>
   );
 }
