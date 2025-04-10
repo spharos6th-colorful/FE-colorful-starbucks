8b32438a532759d3f92c99bdc810b7bf6ea2be67
Dorabang <psa1195@naver.com>
2025-04-10 23:16:40 +0900

    ✅ test: 페이지명이 적힌 header 타입 2가지로 분리
    - PageMainHeader는 MainHeader와 유사하게 장바구니, 검색 페이지 이동 가능한 타입
    - PageSubHeader는 이전 페이지로만 이동이 가능한 타입
    - 강사님과 페이지명은 usePathname과 대조해서 반환해야 할 지에 대한 논의 후 진행 예정

---
+++ src/app/(pageMainHeader)/layout.tsx
@@ -0,0 +1,10 @@
+import PageMainHeader from '@/components/layouts/Header/PageMainHeader';
+
+export default function PageMainHeaderLayout({ children }: { children?: Readonly<React.ReactNode> }) {
+  return (
+    <>
+      <PageMainHeader />
+      {children}
+    </>
+  );
+}
---
+++ src/app/(pageSubHeader)/layout.tsx
@@ -0,0 +1,10 @@
+import PageMSubHeader from '@/components/layouts/Header/PageSubHeader';
+
+export default function PageMainHeaderLayout({ children }: { children?: Readonly<React.ReactNode> }) {
+  return (
+    <>
+      <PageMSubHeader />
+      {children}
+    </>
+  );
+}
