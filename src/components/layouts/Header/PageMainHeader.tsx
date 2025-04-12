17cd6af21493e279f9ca3a1adac8fade2cf20969
Dorabang <psa1195@naver.com>
2025-04-12 23:11:21 +0900

    ✨ feat: pageMainHeader 컴포넌트 추가

--- src/components/layouts/Header/PageMainHeader.tsx
+++ src/components/layouts/Header/PageMainHeader.tsx
@@ -1,3 +1,20 @@
+import Prev from './Prev';
+import Logo from './Logo';
+import UtilWrapper from './UtilWrapper';
+import Search from './Search';
+import Cart from './Cart';
+
 export default function PageMainHeader() {
-  return <header></header>;
+  return (
+    <header className='sticky flex justify-between items-center px-4 py-3 shadow-1'>
+      <Prev />
+
+      <Logo />
+
+      <UtilWrapper>
+        <Search />
+        <Cart />
+      </UtilWrapper>
+    </header>
+  );
 }
