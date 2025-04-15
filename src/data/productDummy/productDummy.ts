e83e749e98387a0406fc1474c6362874fd6255ed
Dora_bang <psa1195@naver.com>
2025-04-15 11:28:18 +0900

    🐛 fix: conflict error 수정

--- src/data/productDummy/productDummy.ts
+++ src/data/productDummy/productDummy.ts
@@ -1,7 +1,7 @@
 import { ProductTypes } from '@/types/products/productTypes';
 
 export const dummyProducts: ProductTypes = {
-  productCode: '1000',
+  productCode: 1000,
   productName: 'SS 플라워 마켓 스탠리 텀블러 591ml',
   description: '부드러운 푸릇빛이 새롭게 담은 플라워 마켓 스탠리 텀블러입니다.',
   productThumbnailUrl: '/images/productThumbnails/1000.png',
