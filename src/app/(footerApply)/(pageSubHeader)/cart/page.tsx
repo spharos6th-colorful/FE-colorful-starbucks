42120928748be72566f58f72e22458e4e610a25a
Dora_bang <psa1195@naver.com>
2025-04-17 15:35:40 +0900

    🐛 fix: 장바구니 비었을 때 화면 추가

--- src/app/(footerApply)/(pageSubHeader)/cart/page.tsx
+++ src/app/(footerApply)/(pageSubHeader)/cart/page.tsx
@@ -1,9 +1,8 @@
-import Link from 'next/link';
-
-import { Body } from '@/components/ui/common';
 import CartForm from '@/components/pages/cart/CartForm';
 import { getCartDatas } from '@/actions/cart-service';
 import CartTerms from '@/components/modules/cart/CartTerms';
+import CartDelivery from '@/components/pages/cart/CartDelivery';
+import EmptyCart from '@/components/ui/cart/EmptyCart';
 
 export default async function CartPage() {
   const cartDatas = await getCartDatas();
@@ -15,41 +14,16 @@ export default async function CartPage() {
 
   if (!cartDatas?.productDetails || cartDatas.productDetails.length === 0) {
     return (
-      <main className='flex flex-col min-h-dvh bg-white'>
-        <section className='p-6 bg-gray-50 flex justify-between items-start'>
-          <Body level={3}>
-            등록된 배송지가 없습니다.
-            <br />
-            배송지를 등록해주세요.
-          </Body>
-          <Link
-            href={'/delivery/create'}
-            className='text-primary-100 text-caption2'
-          >
-            배송지 등록
-          </Link>
-        </section>
-        <section>장바구니가 비어있습니다.</section>
+      <main className='flex flex-col h-full min-h-dvh bg-white'>
+        <CartDelivery />
+        <EmptyCart />
       </main>
     );
   }
 
   return (
     <main className='flex flex-col min-h-dvh bg-white'>
-      {/* 배송지 정보 */}
-      <section className='p-6 bg-gray-50 flex justify-between items-start'>
-        <Body level={3}>
-          등록된 배송지가 없습니다.
-          <br />
-          배송지를 등록해주세요.
-        </Body>
-        <Link
-          href={'/delivery/create'}
-          className='text-primary-100 text-caption2'
-        >
-          배송지 등록
-        </Link>
-      </section>
+      <CartDelivery />
 
       <CartForm action={action} cartDatas={cartDatas} />
 
